a593c75912bc76ebb94ac5ab7be699a37cf2419c
Mr12 <221bst221@gmail.com>
2024-05-20 23:15:22 +0300

    test

--- dfs/MazeGenDFS.py
+++ dfs/MazeGenDFS.py
@@ -1,38 +1,75 @@
 #!/usr/bin/env python3
 
 #Import section
+import numpy as np
 
 
 
 
+class dfs():
+    def __init__(self, graphSize=1):
+        self.Graph = self.generateGraph(np.zeros((graphSize,graphSize)))
+        self.marked = {node:False for node in self.Graph}
+        self.maze = np.zeros((graphSize,graphSize ))
+
+    def generateGraph(self, mazeArrayPoints):
+        '''
+            generate graph adjacency list from np zeros array
+
+            Args:
+                np array
+            
+            Returns:
+                dict with every point to it's all neighbors
+        '''
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        Graph = {}
+        rows, cols = mazeArrayPoints.shape
+        for x in range(rows):
+            for y in range(cols):
+                Graph[(x,y)] = []
+                for neighbor in directions:
+                    Graph[(x,y)].append(tuple(np.add((x,y), neighbor)))
+        return Graph
+
+    def visit(self, node):
+        '''
+            to visit the node
+
+            Args:
+                node : which is a node in our graph to mark it as visited
+            
+            Returns:
+                None
+        '''
+        self.maze[node] = 1
+
+    def dfs(self, startNode):
+        '''
+            a breadth-first search Recursive implementation
+
+            Args : 
+                Graph: Adjacency list
+                v : node 
+        '''
+      
+        self.visit(startNode)
+        self.marked[startNode] = True
+
+        for w in self.Graph[startNode]:
+            if not self.marked[w]:
+                self.dfs(w)
 
 
 
-def visit(node):
-    print(f"{node} has been visited")
 
-def dfs(Graph, root):
-    '''
-        a breadth-first search Recursive implementation
-
-        Args : 
-            Graph: Adjacency list
-            v : node 
-    '''
-    visit(root)
-    marked[root] = True
     
-    for w in Graph[root]:
-        if not marked[w]:
-            dfs(Graph, w)
-
-
 
 
 
 
 if __name__ == '__main__':
     print("Hello world!")
-    graph = {'A': ['B', 'S'], 'B': ['A'], 'S': ['A', 'G', 'C'], 'D': ['C'], 'G': ['S', 'F', 'H'], 'H': ['G', 'E'], 'E': ['C', 'H'], 'F': ['C', 'G'], 'C': ['D', 'S', 'E', 'F']}
-    marked = {node:False for node in graph}
-    dfs(Graph=graph,root='A' )
+    #graph = {'A': ['B', 'S'], 'B': ['A'], 'S': ['A', 'G', 'C'], 'D': ['C'], 'G': ['S', 'F', 'H'], 'H': ['G', 'E'], 'E': ['C', 'H'], 'F': ['C', 'G'], 'C': ['D', 'S', 'E', 'F']}
+    #marked = {node:False for node in graph}
+    #dfs(Graph=graph,root='A' )
